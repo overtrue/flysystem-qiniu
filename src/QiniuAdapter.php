d2f13e93cadf2543224afbed7e85230706a9451f
安正超 <anzhengchao@gmail.com>
2019-08-16 11:20:56 +0800

    Apply fixes from StyleCI (#29)

--- src/QiniuAdapter.php
+++ src/QiniuAdapter.php
@@ -565,11 +565,11 @@ class QiniuAdapter extends AbstractAdapter
     }
 
     /**
-     * Does a UTF-8 safe version of PHP parse_url function
+     * Does a UTF-8 safe version of PHP parse_url function.
      *
-     * @param   string  $url  URL to parse
+     * @param string $url URL to parse
      *
-     * @return  mixed  Associative array or false if badly formed URL.
+     * @return mixed associative array or false if badly formed URL
      *
      * @see     http://us3.php.net/manual/en/function.parse-url.php
      * @since   11.1
@@ -580,7 +580,7 @@ class QiniuAdapter extends AbstractAdapter
 
         // Build arrays of values we need to decode before parsing
         $entities = array('%21', '%2A', '%27', '%28', '%29', '%3B', '%3A', '%40', '%26', '%3D', '%24', '%2C', '%2F', '%3F', '%23', '%5B', '%5D');
-        $replacements = array('!', '*', "'", "(", ")", ";", ":", "@", "&", "=", "$", ",", "/", "?", "#", "[", "]");
+        $replacements = array('!', '*', "'", '(', ')', ';', ':', '@', '&', '=', '$', ',', '/', '?', '#', '[', ']');
 
         // Create encoded URL with special URL characters decoded so it can be parsed
         // All other characters will be encoded
@@ -590,13 +590,12 @@ class QiniuAdapter extends AbstractAdapter
         $encodedParts = parse_url($encodedURL);
 
         // Now, decode each value of the resulting array
-        if ($encodedParts)
-        {
-            foreach ($encodedParts as $key => $value)
-            {
+        if ($encodedParts) {
+            foreach ($encodedParts as $key => $value) {
                 $result[$key] = urldecode(str_replace($replacements, $entities, $value));
             }
         }
+
         return $result;
     }
 }
