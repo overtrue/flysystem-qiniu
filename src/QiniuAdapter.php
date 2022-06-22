4aad39d76cad4d3a0fd2afa32eb132201f1bf7e7
安正超 <anzhengchao@gmail.com>
2022-06-22 03:11:40 +0000

    Fixed #53

--- src/QiniuAdapter.php
+++ src/QiniuAdapter.php
@@ -280,7 +280,7 @@ class QiniuAdapter extends AbstractAdapter
      */
     public function read($path)
     {
-        $contents = file_get_contents($this->getUrl($path));
+        $contents = file_get_contents($this->privateDownloadUrl($path));
 
         return compact('contents', 'path');
     }
@@ -295,7 +295,7 @@ class QiniuAdapter extends AbstractAdapter
     public function readStream($path)
     {
         if (ini_get('allow_url_fopen')) {
-            $stream = fopen($this->getUrl($path), 'r');
+            $stream = fopen($this->privateDownloadUrl($path), 'r');
 
             return compact('stream', 'path');
         }
